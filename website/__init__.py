0fa113aef09480e52b1522a81ae6759e0896b70f
Natan Alves <natan.alves.dev@gmail.com>
2023-04-03 13:03:51 -0400

    implemetation auth and views to init

--- website/__init__.py
+++ website/__init__.py
@@ -3,5 +3,13 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'KKKK'
+    
+    from .views import views
+    from .auth import auth
+    app.register_blueprint(views, url_prefix='/home/')
+    app.register_blueprint(auth, url_prefix='/auth/')
+
+
+
 
     return app
